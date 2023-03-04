7b2540f4a6d63154ce9ef5edef5b7f556f3fe5e2
Kang-Soo Lee <kipacti@gmail.com>
2023-03-04 19:54:42 +0900

    short keys to $window.trigger().

--- docuK-prepare-2.3.js
+++ docuK-prepare-2.3.js
@@ -667,15 +667,15 @@ kipid.docuKProcess=function docuK(kipid, $, docuKI, undefined) {
 <div class="shortkey">
 	Short Keys
 	<ul>
-		<li onclick="kipid.processShortKey({keyCode:'G'.charCodeAt(0)})"><span class="bold underline">G</span>: <span class="bold underline">G</span>o (Fuzzy Search)</li>
-		<li onclick="kipid.processShortKey({keyCode:'K'.charCodeAt(0)})"><span class="bold underline">K</span>: Docu<span class="bold underline">K</span> Log</li>
-		<li onclick="kipid.processShortKey({keyCode:'F'.charCodeAt(0)})"><span class="bold underline">F</span>: <span class="bold underline">F</span>orward Section</li>
-		<li onclick="kipid.processShortKey({keyCode:'D'.charCodeAt(0)})"><span class="bold underline">D</span>: Previous Section</li>
-		<li onclick="kipid.processShortKey({keyCode:'T'.charCodeAt(0)})"><span class="bold underline">T</span>: <span class="bold underline">T</span>able of Contents</li>
-		<li onclick="kipid.processShortKey({keyCode:'R'.charCodeAt(0)})"><span class="bold underline">R</span>: <span class="bold underline">R</span>eferences</li>
-		<li onclick="kipid.processShortKey({keyCode:'L'.charCodeAt(0)})"><span class="bold underline">L</span>: To 전체목록/[<span class="bold underline">L</span>ists]</li>
-		<li onclick="kipid.processShortKey({keyCode:'Z'.charCodeAt(0)})"><span class="bold underline">Z</span>: Tistory comments</li>
-		<li onclick="kipid.processShortKey({keyCode:'X'.charCodeAt(0)})"><span class="bold underline">X</span>: DISQUS comments</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:71})"><span class="bold underline">G</span>: <span class="bold underline">G</span>o (Fuzzy Search)</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:75})"><span class="bold underline">K</span>: Docu<span class="bold underline">K</span> Log</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'F'.charCodeAt(0)})"><span class="bold underline">F</span>: <span class="bold underline">F</span>orward Section</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'D'.charCodeAt(0)})"><span class="bold underline">D</span>: Previous Section</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'T'.charCodeAt(0)})"><span class="bold underline">T</span>: <span class="bold underline">T</span>able of Contents</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'R'.charCodeAt(0)})"><span class="bold underline">R</span>: <span class="bold underline">R</span>eferences</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'L'.charCodeAt(0)})"><span class="bold underline">L</span>: To 전체목록/[<span class="bold underline">L</span>ists]</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'Z'.charCodeAt(0)})"><span class="bold underline">Z</span>: Tistory comments</li>
+		<li onclick="$window.trigger({type:'keydown', keyCode:'X'.charCodeAt(0)})"><span class="bold underline">X</span>: DISQUS comments</li>
 	</ul>
 </div>`);
 	docuK.after(`<div class="copyright"><ul>
