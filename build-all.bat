ab51c40e65652ea9b6cfdd1fedac0624d636c442
kipid <kipacti@gmail.com>
2025-04-13 13:41:24 +0900

    Update build-all.bat

--- build-all.bat
+++ build-all.bat
@@ -1,6 +1,8 @@
 node .\esbuild.config.js
 
 REM js 파일들을 CDN 폴더로 복사
+copy /y "docuK-2.3.css" "C:\Recoeve\src\main\java\recoeve\db\CDN\"
+
 copy /y "docuK-postProcess-2.3.js" "C:\Recoeve\src\main\java\recoeve\db\CDN\"
 
 copy /y "docuK-postProcess-2.3.js.map" "C:\Recoeve\src\main\java\recoeve\db\CDN\"
